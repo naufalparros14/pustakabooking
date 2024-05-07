30d86ca1d643eb5396c64a4cde54fdf529085c52
mohammadtaufiqabdillah <tfkabdllh@gmail.com>
2024-05-07 16:35:26 +0700

    pertemuan7

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,9 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload["libraries"] = array("form_validation");
+$autoload['libraries'] = array('form_validation', 'session',
+'database');
+
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +134,5 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('ModelUser', 'ModelBuku');
+
